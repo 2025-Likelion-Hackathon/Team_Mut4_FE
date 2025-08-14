db9e8bbfa5b82614a350925d256eaa9e717edd23
yongchane <202021113@sangmyung.kr>
2025-08-14 16:00:19 +0900

    haeder 변경

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="sticky w-full h-[70px] z-50 top-0  flex justify-between items-center p-4 text-black bg-white shadow-md">
-      <div className="text-lg font-bold">App Logo</div>
+    <div className="sticky w-full h-[70px] z-50 top-0  flex justify-between items-center p-4 text-black text-[19px] font-bold bg-white shadow-md">
+      <div className="">홈</div>
       {/* Placeholder for text top-right */}
-      <div className="text-sm">User Info</div>
+      <div className="">지역</div>
     </div>
   );
 };

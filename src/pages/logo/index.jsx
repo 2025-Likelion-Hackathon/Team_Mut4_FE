16e4761aeb40f31e4f39ebf87ea6ab57af707d87
yongchane <202021113@sangmyung.kr>
2025-08-23 22:17:21 +0900

    로고 화면 자동 넘김 추가

--- src/pages/logo/index.jsx
+++ src/pages/logo/index.jsx
@@ -1,8 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../../assets/Logo.svg?react";
 
 function LogoPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/auth");
+    }, 2000); // 2초 후 자동 이동
+
+    // 컴포넌트 언마운트 시 타이머 정리
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
   return (
     <div className="flex justify-center items-center h-screen bg-white">
       <Link to="/auth">

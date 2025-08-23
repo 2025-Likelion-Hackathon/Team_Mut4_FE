c8a94d9a387d6803d29c5985e1d36845f2b049e1
sumin <breath0312@gmail.com>
2025-08-24 03:36:23 +0900

    리뷰 버튼 수정

--- src/pages/restaurantDetails/components/ReviewButton.jsx
+++ src/pages/restaurantDetails/components/ReviewButton.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { useNavigate, useParams } from 'react-router-dom';
 
-// isLocal 값에 따라 버튼 스타일을 동적으로 변경합니다.
 const Button = styled.button`
   width: calc(100% - 2rem);
   margin: 1rem;
@@ -28,13 +27,11 @@ const ReviewButton = ({ type, isLocal }) => {
   const { id } = useParams();
 
   const handleNavigate = () => {
-    // isLocal이 false이면 아무 동작도 하지 않습니다.
     if (!isLocal) return;
     navigate(`/${type}/${id}/write`);
   };
 
   return (
-    // isLocal이 false이면 disabled 속성이 true가 됩니다.
     <Button onClick={handleNavigate} disabled={!isLocal}>
       {isLocal ? '이 장소 리뷰쓰기' : '현지인 인증 필요'}
     </Button>

452226bc60d0dd7cb1609d36e7cffed9aca545cd
yongchane <202021113@sangmyung.kr>
2025-08-24 18:28:11 +0900

    로고 페이지 수정

--- src/pages/logo/index.jsx
+++ src/pages/logo/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import Logo from "../../assets/Logo.svg?react";
+import Logo from "../../assets/BLogo.svg?react";
 
 function LogoPage() {
   const navigate = useNavigate();
@@ -8,16 +8,16 @@ function LogoPage() {
   useEffect(() => {
     const timer = setTimeout(() => {
       navigate("/auth");
-    }, 2000); // 2초 후 자동 이동
+    }, 1500); // 2초 후 자동 이동
 
     // 컴포넌트 언마운트 시 타이머 정리
     return () => clearTimeout(timer);
   }, [navigate]);
 
   return (
-    <div className="flex justify-center items-center h-screen bg-white">
-      <Link to="/auth">
-        <Logo />
+    <div className="w-full h-screen bg-white flex justify-center items-center pt-[40%]">
+      <Link to="/auth" className=" w-full  h-full ">
+        <Logo className="w-full h-full object-contain" />
       </Link>
     </div>
   );
